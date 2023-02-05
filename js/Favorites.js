23cd0acca576b41e81aa1efb27ce22ded396181a
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-05 07:46:58 -0300

    conhecendo o localstorage

--- js/Favorites.js
+++ js/Favorites.js
@@ -5,26 +5,16 @@ export class Favorites {
   }
 
   load() {
-    this.entries = [
-      {
-        login: "maykbrito",
-        name: "Mayk Brito",
-        public_repos: "76",
-        followers: "120000",
-      },
-      {
-        login: "diego3g",
-        name: "Diego Fernandes",
-        public_repos: "76",
-        followers: "120000",
-      },
-    ];
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
   }
 
   delete(user) {
     const filteredEntries = this.entries.filter(
       (entry) => entry.login !== user.login
     );
+
+    this.entries = filteredEntries;
+    this.update();
   }
 }
 
