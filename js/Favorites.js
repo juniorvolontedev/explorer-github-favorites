422e4093e073bc870aebade1a38a1b6022930b35
Junior Volonte <juniorvolontedev@gmail.com>
2023-02-05 08:37:15 -0300

    ajustes finais, revisao e proximos passos

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`;
-
-    return fetch(endpoint)
-      .then((data) => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers,
-      }));
-  }
-}
+import { GithubUser } from "./GithubUser.js";
 
 export class Favorites {
   constructor(root) {
@@ -29,6 +16,12 @@ export class Favorites {
 
   async add(username) {
     try {
+      const userExistis = this.entries.find((entry) => entry.login == username);
+
+      if (userExistis) {
+        throw new Error("Usuário já cadastrado");
+      }
+
       const user = await GithubUser.search(username);
       console.log(user);
 
@@ -85,6 +78,8 @@ export class FavoritesView extends Favorites {
       ).src = `https://github.com/${user.login}.png`;
 
       row.querySelector(".user img ").alt = `Imagem de ${user.name}`;
+
+      row.querySelector(".user a").href = `https://github.com/${user.login}`;
 
       row.querySelector(".user p").textContent = user.name;
 
